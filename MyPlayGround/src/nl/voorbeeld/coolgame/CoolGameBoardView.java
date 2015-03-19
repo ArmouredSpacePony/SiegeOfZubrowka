46335a1963122693e21e16b82a8dc2dbeaf27f71
ArmouredSpacePony <matthijs_piekartz@hotmail.com>
2015-03-19 10:16:17 +0100

    update v. Joep nav code cleaning

--- MyPlayGround/src/nl/voorbeeld/coolgame/CoolGameBoardView.java
+++ MyPlayGround/src/nl/voorbeeld/coolgame/CoolGameBoardView.java
@@ -3,18 +3,16 @@ package nl.voorbeeld.coolgame;
 import nl.saxion.act.playground.R;
 import nl.saxion.act.playground.view.GameBoardView;
 import nl.saxion.act.playground.view.SpriteCache;
-import nl.voorbeeld.coolgame.objects.Leaf;
-import nl.voorbeeld.coolgame.objects.Rock;
+import nl.voorbeeld.coolgame.objects.Enemy;
 import nl.voorbeeld.coolgame.objects.Player;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 
 /**
- * A view on the CoolGame game board.
+ * A view on the Siege of Zubrowka game board.
  * 
- * @author Jan Stroet
- * @author Paul de Groot
+ * @author team Wild Ponies
  */
 public class CoolGameBoardView extends GameBoardView {
 	private static final String TAG = "GameView";
@@ -39,23 +37,24 @@ public class CoolGameBoardView extends GameBoardView {
 	 * Loads all images that will be used for the game.
 	 */
 	private void initGameView() {
-		if (!isInEditMode()) {
-			Log.d(TAG, "Loading all images");
+		Log.d(TAG, "Loading all images");
 
-			SpriteCache spriteCache = SpriteCache.getInstance();
-			spriteCache.setContext(this.getContext());
+		SpriteCache spriteCache = SpriteCache.getInstance(); 
+		spriteCache.setContext(this.getContext());		
 
-			// Load the 'empty' cell bitmap and tell the tile view that this is
-			// the
-			// image to use for cells without GameObject
-			spriteCache.loadTile("empty", R.drawable.cell);
-			setEmptyTile("empty");
+		// Load the 'empty' cell bitmap and tell the tile view that this is the
+		// image to use for cells without GameObject
+		spriteCache.loadTile("empty", R.drawable.grasstile1);
+		spriteCache.loadTile("wall", R.drawable.concrete1);
+		setEmptyTile("empty");
+		
+		// Load the images for the GameObjects
 
-			// Load the images for the GameObjects
-			spriteCache.loadTile(Leaf.LEAF_IMAGE, R.drawable.leaf);
-			spriteCache.loadTile(Rock.ROCK_IMAGE, R.drawable.rock);
-			spriteCache.loadTile(Rock.RED_ROCK_IMAGE, R.drawable.rock2);
-			spriteCache.loadTile(Player.WOMBAT_IMAGE, R.drawable.wombat);
-		}
+		spriteCache.loadTile(Player.PLAYER_IMAGE, R.drawable.player);
+		spriteCache.loadTile(Enemy.Enemy1_IMAGE, R.drawable.civilian3);
+		spriteCache.loadTile(Enemy.Enemy2_IMAGE, R.drawable.civilian4);
+		spriteCache.loadTile(Enemy.Enemy3_IMAGE, R.drawable.civilian5);
+		// Load the image for the bullet (projectile)
+		spriteCache.loadTile("bullet", R.drawable.bullet);
 	}
 }
