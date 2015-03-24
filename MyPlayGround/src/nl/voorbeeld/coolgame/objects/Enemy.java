934fbda826dc8924dcd1d1655d7ccc338caba9d2
shandrolis <joepvdstaai@live.nl>
2015-03-24 10:33:52 +0100

    Update Enemy.java

--- MyPlayGround/src/nl/voorbeeld/coolgame/objects/Enemy.java
+++ MyPlayGround/src/nl/voorbeeld/coolgame/objects/Enemy.java
@@ -1,48 +1,76 @@
 package nl.voorbeeld.coolgame.objects;
 
+
+
+import java.util.Random;
+
 import android.util.Log;
 import nl.saxion.act.playground.model.GameBoard;
 import nl.saxion.act.playground.model.GameObject;
 import nl.voorbeeld.coolgame.CoolGame;
+import nl.saxion.act.playground.model.Game;
 
-/**
- * A Rock object blocks the movement of the Wombat and Leaf.
- * If the user touches a rock, it toggles between being red or grey, but this
- * is only a visual effect.
- * 
- * @author Paul de Groot
- */
-public class Rock extends GameObject {
-	public static final String ROCK_IMAGE = "Rock";
-	public static final String RED_ROCK_IMAGE = "RockTwo";
-
-	/** True if the block should be shown red, false if it's grey. */
-	private boolean redBlock;
-
+public class Enemy extends GameObject implements Runnable {
+	public static final String Enemy1_IMAGE = "civilian3";
+	public static final String Enemy2_IMAGE = "civilian4";
+	public static final String Enemy3_IMAGE = "civilian5";
+	private String currentImage;
+	
 	/**
-	 * Constructs a rock.
+	 * Constructs an enemy.
 	 */
-	public Rock(boolean red) {
+	public Enemy() {
 		super();
-		this.redBlock = red;
+		Random r = new Random();
+		int getal = r.nextInt(3)+2;
+		currentImage=("Enemy"+(getal)+"_IMAGE");
 	}
-
-	/** Returns the ImageId of the image to show. */
+	
+	  /** Returns the ImageId of the image to show. */
+	
 	@Override
 	public String getImageId() {
-		if (redBlock) {
-			return RED_ROCK_IMAGE;
-		} else {
-			return ROCK_IMAGE;
+		if(currentImage.equals("civilian3")){
+			return Enemy1_IMAGE;}
+		else if(currentImage.equals("civilian4")){
+			return Enemy2_IMAGE;
+		}else{
+			return Enemy3_IMAGE;
 		}
+		
 	}
 
 	/** Called when the user touched this rock. */
 	@Override
 	public void onTouched(GameBoard gameBoard) {
-		// Toggle the red block boolean
-		Log.d(CoolGame.TAG, "Touched rock");
-		redBlock = !redBlock;
+		// placeholder until movement functions
+		Log.d(CoolGame.TAG, "Touched Enemy");
+		callMovement(gameBoard);
+		
+		//redBlock = !redBlock;
 		gameBoard.updateView();
+	}
+	
+	public void callMovement(GameBoard gameBoard){
+		
+			Log.d(CoolGame.TAG, "Moved Enemy");
+			// player moves to the right
+			int newPosX = getPositionX();
+			int newPosY = getPositionY()+1;
+
+			// If new position crosses border, call gameover
+			if(newPosY > 16){
+				((CoolGame)gameBoard.getGame()).gameOver();
+			}
+			// Move player to the new position and redraw the app
+			gameBoard.moveObject(this, newPosX, newPosY);
+			gameBoard.updateView();
+		
+	}
+
+	@Override
+	public void run() {
+		callMovement(Game.gameBoard);
+		
 	}
 }
