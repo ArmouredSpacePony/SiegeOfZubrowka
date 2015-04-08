dfaab5d4ce71f4f35c98abbdf60f883f4af26282
AC2222 <aukecrooijmans@gmail.com>
2015-04-08 21:25:44 +0200

    Update Savegame.java

--- MyPlayGround/src/nl/voorbeeld/SoZ/Savegame.java
+++ MyPlayGround/src/nl/voorbeeld/SoZ/Savegame.java
@@ -1,10 +1,23 @@
 package nl.voorbeeld.SoZ;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+
+import nl.voorbeeld.SoZ.objects.Muur;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 
 public class Savegame {
 
@@ -13,14 +26,14 @@ public class Savegame {
 	private int level;
 
 	private int points;
-	
+
 	private String equiptWep;
 
 	private boolean ak;
 	private boolean handgun;
 	private boolean shotgun;
 	private boolean sniper;
-
+	private File file;
 	private int bodyArmor;
 
 	private int guards;
@@ -28,16 +41,20 @@ public class Savegame {
 
 	private final int maxMuur = 9;
 
-	private ArrayList <Integer> muur = new ArrayList<Integer>();
+	private ArrayList<Integer> muur = new ArrayList<Integer>();
 
+	private Activity activity;
 
+	public Savegame(Activity activity) {
+		this.activity = activity;
+		file = new File(activity.getApplicationContext().getFilesDir(),
+				"savegame.txt");
 
-	public Savegame() {
 		level = 1;
 
 		points = 0;
-		
-		equiptWep="ak";
+
+		equiptWep = "ak";
 
 		ak = true;
 		handgun = false;
@@ -48,7 +65,7 @@ public class Savegame {
 
 		guards = 0;
 		dogs = 0;
-		
+
 		muur.add(100);
 		muur.add(100);
 		muur.add(100);
@@ -58,45 +75,40 @@ public class Savegame {
 		muur.add(100);
 		muur.add(100);
 		muur.add(100);
-/*
-		for (int i : muur) {
-			muur[i] = 100;
-			Log.d("lol", ""+muur[i]);
-		}*/
+		/*
+		 * for (int i : muur) { muur[i] = 100; Log.d("lol", ""+muur[i]); }
+		 */
 	}
 
 	public void leesSaveGameUitFile() {
-		File file = new File("savegame.txt");
 		try {
 			in = new Scanner(file);
-
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			return;
 		}
 
 		int loadedLevel;
 		int loadedPoints;
-		String loadedEquiptWep = equiptWep;
+		String loadedEquiptWep;
 		boolean loadedAk;
-		boolean loadedHandgun;
 		boolean loadedShotgun;
-		boolean loadedSniper;
-		int loadedBodyArmor;
 		int loadedGuards;
 		int loadedDogs;
-		int[] loadedMuur = new int[maxMuur];
+		ArrayList<Integer> loadedMuuren = new ArrayList<Integer>(9);
 
 		in.useDelimiter("/");
 
-		if (in.hasNext() && in.hasNextInt()) {
-			loadedLevel = Integer.parseInt(in.next());
+		if (in.hasNext()) {
+			String input = in.next();
+			Log.i("saveing", input);
+			loadedLevel = Integer.parseInt(input);
 			if (loadedLevel < 1) {
 				return;
 			}
 		} else {
 			return;
 		}
-		if (in.hasNext() && in.hasNextInt()) {
+		if (in.hasNext()) {
 			loadedPoints = Integer.parseInt(in.next());
 			if (loadedPoints < 0) {
 				return;
@@ -104,28 +116,31 @@ public class Savegame {
 		} else {
 			return;
 		}
-		if (in.hasNext()){
-			if (in.next().equals("ak")) {
-
+		if (in.hasNext()) {
+			String i = in.next();
+			Log.i("saveing", "equip");
+			if (i.equals("ak")) {
+				Log.i("saveing", "ak");
 				loadedEquiptWep = "ak";
-			} else if (in.next().equals("handgun")) {
+			} else if (i.equals("handgun")) {
 				loadedEquiptWep = "handgun";
-			} 
-			else if (in.next().equals("shotgun")) {
+			} else if (i.equals("shotgun")) {
 				loadedEquiptWep = "shotgun";
-			}
-			else if (in.next().equals("sniper")) {
+			} else if (i.equals("sniper")) {
 				loadedEquiptWep = "sniper";
-			}else {
+			} else {
 				return;
 			}
+		}else{
+			return;
 		}
-		
-		if (in.hasNext()) {
-			if (in.next().equals("true")) {
 
+		if (in.hasNext()) {
+			String i = in.next();
+			if(i.equals("true")){
+				Log.i("saveing", "ownes ak");
 				loadedAk = true;
-			} else if (in.next().equals("false")) {
+			} else if (i.equals("false")) {
 				loadedAk = false;
 			} else {
 				return;
@@ -133,23 +148,13 @@ public class Savegame {
 		} else {
 			return;
 		}
+		
 		if (in.hasNext()) {
-			if (in.next().equals("true")) {
-
-				loadedHandgun = true;
-			} else if (in.next().equals("false")) {
-				loadedHandgun = false;
-			} else {
-				return;
-			}
-		} else {
-			return;
-		}
-		if (in.hasNext()) {
-			if (in.next().equals("true")) {
-
+			String i = in.next();
+			if(i.equals("true")){
+				Log.i("saveing", "ownes ak");
 				loadedShotgun = true;
-			} else if (in.next().equals("false")) {
+			} else if (i.equals("false")) {
 				loadedShotgun = false;
 			} else {
 				return;
@@ -157,27 +162,17 @@ public class Savegame {
 		} else {
 			return;
 		}
-		if (in.hasNext()) {
-			if (in.next().equals("true")) {
-
-				loadedSniper = true;
-			} else if (in.next().equals("false")) {
-				loadedSniper = false;
-			} else {
-				return;
-			}
-		} else {
-			return;
-		}
-		if (in.hasNext() && in.hasNextInt()) {
+		/*if (in.hasNext()) {
+			Log.i("saveing", "armor");
 			loadedBodyArmor = Integer.parseInt(in.next());
 			if (loadedBodyArmor < 0) {
 				return;
 			}
 		} else {
 			return;
-		}
-		if (in.hasNext() && in.hasNextInt()) {
+		}*/
+		if (in.hasNext()) {
+			Log.i("saveing", "jevnvobe");
 			loadedGuards = Integer.parseInt(in.next());
 			if (loadedGuards < 0) {
 				return;
@@ -185,7 +180,7 @@ public class Savegame {
 		} else {
 			return;
 		}
-		if (in.hasNext() && in.hasNextInt()) {
+		if (in.hasNext()) {
 			loadedDogs = Integer.parseInt(in.next());
 			if (loadedDogs < 0) {
 				return;
@@ -193,49 +188,61 @@ public class Savegame {
 		} else {
 			return;
 		}
-		for (int i : loadedMuur) {
-			if (in.hasNext() && in.hasNextInt()) {
-				loadedMuur[i] = Integer.parseInt(in.next());
-				if (loadedMuur[i] < 0) {
-					return;
-				}
-			} else {
-				return;
+		int i=0;
+		while (i < muur.size()) {
+			if (in.hasNext()) {
+				Log.i("array", i+"");
+				loadedMuuren.add(Integer.parseInt(in.next()));
+				
 			}
+			i++;
+			Log.i("saveing", "loaded muur");
 		}
 
+		Log.i("saveing", "loaded");
+		
 		level = loadedLevel;
 
 		points = loadedPoints;
-		
-		equiptWep =loadedEquiptWep;
-		
+
+		equiptWep = loadedEquiptWep;
+
 		ak = loadedAk;
-		handgun = loadedHandgun;
+		
 		shotgun = loadedShotgun;
-		sniper = loadedSniper;
+		
 
 		guards = loadedGuards;
 		dogs = loadedGuards;
 
-	//	for (int i : muur) {
-	//		muur.get(i) = loadedMuur[1];
-	//	}
+		int i2 = 0;
+		while (i2< muur.size()) {
+			Log.i("array2", i2+"");
+		muur.set(i2, loadedMuuren.get(i2));
+		i2++;
+		}
+		
+		Log.i("point", ""+points);
 		return;
 	}
 
 	public void schrijfSaveGame() {
-		File file = new File("savegame.txt");
+		String save = (level + "/" + points + "/" + equiptWep + "/" + ak + "/"
+				+ shotgun + "/" + guards + "/"
+				+ dogs +  "/" + muur.get(0) + "/" + muur.get(1) + "/"
+				+ muur.get(2) + "/" + muur.get(3) + "/" + muur.get(4) + "/"
+				+ muur.get(5) + "/" + muur.get(6) + "/" + muur.get(7) + "/" + muur
+				.get(8));
 		try {
-			PrintWriter out = new PrintWriter(file);
-			out.print(level + "/" + points + "/" + equiptWep + "/" + ak + "/" + handgun + "/"
-					+ shotgun + "/" + sniper + "/" + guards + "/" + dogs + "/"
-					+ "/" + muur.get(0)+ "/" + muur.get(1)+ "/" + muur.get(2)+ "/" + muur.get(3)
-					+ "/" + muur.get(4)+ "/" + muur.get(5)+ "/" + muur.get(6)+ "/" + muur.get(7)
-					+ "/" + muur.get(8));
-			out.close();
-		} catch (FileNotFoundException e) {
+			file.delete();
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(
+					file));
+			bufferedWriter.write(save);
+			bufferedWriter.close();
 
+			Log.i("saveing", "saved");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
@@ -268,10 +275,9 @@ public class Savegame {
 	 *            the points
 	 */
 	public void setPoints(int points) {
+		
 		this.points = points;
 	}
-	
-	
 
 	public String getEquiptWep() {
 		return equiptWep;
@@ -368,9 +374,9 @@ public class Savegame {
 	 *            the number of guards left
 	 */
 	public void setGuards(int guards) {
-		if(guards < 0){
+		if (guards < 0) {
 			this.guards = 0;
-		}else{
+		} else {
 			this.guards = guards;
 		}
 	}
